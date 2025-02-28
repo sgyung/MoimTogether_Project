821536cb77f74d407ed90637fc60a8273326c789
SEONKYUNG <tjsrud228@naver.com>
2025-02-28 16:43:48 +0900

    deploy: 무중단 배포 스크립트 작성 및 yml수정

--- src/main/java/com/project/moimtogether/config/SecurityConfiguration.java
+++ src/main/java/com/project/moimtogether/config/SecurityConfiguration.java
@@ -61,7 +61,8 @@ public class SecurityConfiguration {
                         "/",
                         "/v3/api-docs/**",
                         "/api/seoul-data",
-                        "/api/places").permitAll() // 인증 필요 없음
+                        "/api/places",
+                        "/api/environment").permitAll() // 인증 필요 없음
                 .anyRequest().authenticated() // 나머지는 인증 필요
         );
 
