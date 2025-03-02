381e649e17ba1ddcad4c28707d8017a4dc2edaf8
SEONKYUNG <tjsrud228@naver.com>
2025-03-02 10:33:39 +0900

    deploy : deploy 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -50,7 +50,8 @@ do
         echo "서비스 기동 실패 (port: $TARGET_PORT)"
 
         # 신규 서비스 종료 (실패한 버전 종료)
-        NEW_PID=$(lsof -ti tcp:$TARGET_PORT)
+        NEW_PID=$(sudo lsof -ti :$TARGET_PORT)
+        echo "> 신규 서비스 PID: $NEW_PID"
         if [ ! -z "$NEW_PID" ]; then
             echo "> 배포 실패로 신규 서비스 종료 (PID: $NEW_PID)"
             kill -15 $NEW_PID
